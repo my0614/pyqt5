2da7e78b7929700163e5eb3dd8a1be6117acedff
my0614 <aqi222@naver.com>
2020-08-15 17:05:49 +0900

    ADD :  시간 일의 자리일때 0추가 하기

--- mirror_project/main.py
+++ mirror_project/main.py
@@ -16,29 +16,46 @@ def wheather_crolling(self):
         self.Weather_1.setStyleSheet("font : 75 15pt; background-color : white; border-bottom-right-radius: 15px; border-bottom-left-radius: 15px;")
         self.Weather.setText( temp[0].text + "℃\n")
         self.Weather_1.setText(temp_sub[0].text)
-        
+
         # 현재시간 가지고 오기
         self.timer = QTimer()
         self.timer.start(1000)# 1초마다 가져오기
         self.timer.timeout.connect(self.time_crolling)
 
 def time_crolling(self):
-        
+    clock =0
     clock = datetime.today()
     self.now_time.setStyleSheet("font : 75 25pt; background-color : white; border-radius : 15px;")
     if clock.hour >= 12:
-        if clock.second >= 10:
+        if clock.second >= 10 and clock.minute >=10:
             self.now_time.setText("PM " + str(clock.hour) + " : " + str(clock.minute) + " : " + str(clock.second))
-        else:
-            self.now_time.setText(str(clock.hour) + " : " + str(clock.minute) + " : 0" + str(clock.second))
+        elif clock.second >= 10 and clock.minute <= 10:
+            self.now_time.setText("PM " + str(clock.hour) + " : 0" + str(clock.minute) + " : " + str(clock.second))
+        elif clock.second <= 10 and clock.minute >= 10:
+            self.now_time.setText("PM " + str(clock.hour) + " : " + str(clock.minute) + " : 0" + str(clock.second))
+        if clock.second <= 10 and clock.minute <= 10:
+            self.now_time.setText("PM " + str(clock.hour) + " : 0" + str(clock.minute) + " : 0" + str(clock.second))
+
     else:
-        if clock.second >= 10:
-            self.now_time.setText("PM " + str(clock.hour) + " : " + str(clock.minute) + " : " + str(clock.second))
-        else:
-            self.now_time.setText(str(clock.hour) + " : " + str(clock.minute) + " : 0" + str(clock.second))
+        if clock.second >= 10 and clock.minute >=10:
+            self.now_time.setText("AM " + str(clock.hour) + " : " + str(clock.minute) + " : " + str(clock.second))
+        elif clock.second >= 10 and clock.minute <= 10:
+            self.now_time.setText("AM " + str(clock.hour) + " : " + str(clock.minute) + " : " + str(clock.second))
+        elif clock.second <= 10 and clock.minute >= 10:
+            self.now_time.setText("AM " + str(clock.hour) + " : " + str(clock.minute) + " : " + str(clock.second))
+        if clock.second <=10 and clock.minute <= 10:
+            self.now_time.setText("AM " + str(clock.hour) + " : " + str(clock.minute) + " : " + str(clock.second))        
+
+#def news_crolling(self):
+ #   news_source = requests.get("https://www.naver.com/").text
+  #  news_soup = BeautifulSoup(news_source, "html.parser")
+   # temp = news_soup.select(".keyword")
+    #self.news.setText(temp[].text)
 
 
-#Ui_MainWindow.basic = basic
+
+
+
 Ui_MainWindow.time_crolling = time_crolling
 Ui_MainWindow.wheather_crolling = wheather_crolling
 
