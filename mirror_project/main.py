4a40b9ec2a64b48437294f2aa94dc9b2bf0e7d6a
my0614 <aqi222@naver.com>
2020-08-15 22:48:41 +0900

    ADD :  new topic crolling success

--- mirror_project/main.py
+++ mirror_project/main.py
@@ -17,7 +17,7 @@ def wheather_crolling(self):
         self.Weather.setText( temp[0].text + "℃\n")
         self.Weather_1.setText(temp_sub[0].text)
 
-        # 현재시간 가지고 오기
+        # 반복기능
         self.timer = QTimer()
         self.timer.start(1000)# 1초마다 가져오기
         self.timer.timeout.connect(self.time_crolling)
@@ -44,29 +44,39 @@ def time_crolling(self):
         elif clock.second <= 10 and clock.minute >= 10:
             self.now_time.setText("AM " + str(clock.hour) + " : " + str(clock.minute) + " : " + str(clock.second))
         if clock.second <=10 and clock.minute <= 10:
-            self.now_time.setText("AM " + str(clock.hour) + " : " + str(clock.minute) + " : " + str(clock.second))        
+            self.now_time.setText("AM " + str(clock.hour) + " : " + str(clock.minute) + " : " + str(clock.second))  
+        # new_crolling 반복      
+        self.timer = QTimer()
+        self.timer.start(10000)# 10초마다 가져오기
+        self.timer.timeout.connect(self.news_crolling)
 
-#def news_crolling(self):
- #   news_source = requests.get("https://www.naver.com/").text
-  #  news_soup = BeautifulSoup(news_source, "html.parser")
-   # temp = news_soup.select(".keyword")
-    #self.news.setText(temp[].text)
+def news_crolling(self):
+    news_source =requests.get("https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%89%B4%EC%8A%A4%ED%86%A0%ED%94%BD&oquery=%EB%84%A4%EC%9D%B4%EB%B2%84+%EC%8B%A4%EC%8B%9C%EA%B0%84+%EA%B2%80%EC%83%89%EC%96%B4+%EC%88%9C%EC%9C%84&tqi=UzQlmwp0J14ssflcJlsssssssHR-396770").text
+    news_soup = BeautifulSoup(news_source, "html.parser")
+    sub_key = news_soup.select("span.tit")
 
-
-
+    index =0
+    key=1
+    for key in sub_key:
+        index += 1
+        self.news.append("["+ str(index) + "]  "+str(key))
+        if index >= 5:
+            break
+        
 
 
 Ui_MainWindow.time_crolling = time_crolling
 Ui_MainWindow.wheather_crolling = wheather_crolling
+Ui_MainWindow.news_crolling = news_crolling
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
-   # ui.basic()
     ui.setupUi(MainWindow)
     ui.wheather_crolling()
     ui.time_crolling()
+    ui.news_crolling()
     MainWindow.show()
     sys.exit(app.exec_())
