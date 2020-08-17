8c0b3cc9a18033eb494516bede043aed0f4115ea
my0614 <aqi222@naver.com>
2020-08-17 23:26:15 +0900

    ADD : 개인프로필 추가

--- mirror_project/main.py
+++ mirror_project/main.py
@@ -18,23 +18,16 @@ def wheather_crolling(self):
         wheather_soup = BeautifulSoup(wheather_source, "html.parser")
         temp = wheather_soup.select(".todaytemp")
         temp_sub = wheather_soup.select(".cast_txt")
-        self.Weather.setStyleSheet("background-color : white;font : 75 22pt; border-top-right-radius: 15px; border-top-left-radius: 15px;")
-        self.Weather_1.setStyleSheet("font : 75 15pt; background-color : white; border-bottom-right-radius: 15px; border-bottom-left-radius: 15px;")
+        self.Weather.setStyleSheet("background-color : rgb(255,255,255); font : 75 22pt; border-top-right-radius: 15px; border-top-left-radius: 15px;")
+        self.Weather_1.setStyleSheet("background-color : rgb(255,255,255); font : 75 15pt;border-bottom-right-radius: 15px; border-bottom-left-radius: 15px;")
         self.Weather.setText( temp[0].text + "℃\n")
         self.Weather_1.setText(temp_sub[0].text)
-
-
-#def diner(self):
- #   diner_source = requests.get("https://www.dsm-dms.com/").text
-  #  diner_soup = BeautifulSoup(diner_source, "html.parser")
-
-
 
 def time_crolling(self):
     
     clock =0
     clock = datetime.today()
-    self.now_time.setStyleSheet("font : 75 25pt; background-color : white; border-radius : 15px;")
+    self.now_time.setStyleSheet("background-color :  rgb(255, 167, 140); font : 75 25pt;  border-radius : 15px; ")
     if clock.hour >= 12:
         if clock.second >= 10 and clock.minute >=10:
             self.now_time.setText("PM " + str(clock.hour) + " : " + str(clock.minute) + " : " + str(clock.second))
@@ -69,53 +62,79 @@ def time_crolling(self):
         
 
 def news_crolling(self):
-    self.news.setStyleSheet("font : 75 12pt;")
+    self.topic.setStyleSheet("font : 75 12pt; ")
     news_source =requests.get("https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%89%B4%EC%8A%A4%ED%86%A0%ED%94%BD&oquery=%EB%84%A4%EC%9D%B4%EB%B2%84+%EC%8B%A4%EC%8B%9C%EA%B0%84+%EA%B2%80%EC%83%89%EC%96%B4+%EC%88%9C%EC%9C%84&tqi=UzQlmwp0J14ssflcJlsssssssHR-396770").text
     news_soup = BeautifulSoup(news_source, "html.parser")
     sub_key = news_soup.select("span.tit")
 
     index = 0
     key=2
-    self.news.append("뉴스")
+    self.topic.append("뉴스")
     for key in sub_key:
         index += 1
-        self.news.append("["+ str(index) + "]  "+str(key))
+        self.topic.append("["+ str(index) + "]  "+str(key))
         if index >= 5:
             break
     
     index = 0
     key = 1
     cnt = 1
-    self.news.append("\n연예/스포츠")
+    self.topic.append("\n연예/스포츠")
     for key in sub_key:
         index += 1
         if index >=11:
-            self.news.append("["+ str(cnt) + "]  "+str(key))
+            self.topic.append("["+ str(cnt) + "]  "+str(key))
             cnt += 1
             if index >= 15:
                 break
     
    
 def baseball_crolling(self):
-    self.baseball.setStyleSheet("font : 75 12pt;")
+    self.sports.setStyleSheet("font : 75 12pt;")
     baseball_source = requests.get("https://sports.media.daum.net/sports/baseball/").text
     baseball_soup = BeautifulSoup(baseball_source,"html.parser")
     rank = baseball_soup.select("span.txt_team")
-    self.baseball.append("2020 프로야구 순위\n")
+    self.sports.append("2020 프로야구 순위")
     
     index = 0
     for i in rank:
         index +=1
         #print(str(index))
-        self.baseball.append(str(index) +"   " + str(i))
+        self.sports.append(str(index)+"등 "+str(i))
         if index >= 10:
             break
+
+
+def Myinfo(self):
+    self.myinfo.append("기본 프로필")
+    self.myinfo.append("2002.06.14 김민영")
+    self.myinfo.append("대덕소프트웨어마이스터")
+    self.myinfo.append("임베디드SW 재학")
+
+
+def diner(self):
+    diner_source = requests.get("https://www.dsm-dms.com/").text
+    diner_soup = BeautifulSoup(diner_source, "html.parser")
+    blackfirst = diner_soup.select("span.meal--card--temp")
+    self.diner_moring.append("아침")
+
+    cnt = 0
+    for i in blackfirst:
+        cnt += 1
+        print(cnt)
+        self.diner_moring.append(str(i))
+        if index >= 2:
+            break
+
+
 
 Ui_MainWindow.timer_event = timer_event
 Ui_MainWindow.wheather_crolling = wheather_crolling
 Ui_MainWindow.time_crolling = time_crolling
 Ui_MainWindow.news_crolling = news_crolling
 Ui_MainWindow.baseball_crolling =baseball_crolling
+Ui_MainWindow.diner = diner
+Ui_MainWindow.Myinfo = Myinfo
 
 
 if __name__ == "__main__":
@@ -129,5 +148,7 @@ if __name__ == "__main__":
     ui.time_crolling()
     ui.news_crolling()
     ui.baseball_crolling()
+    ui.diner()
+    ui.Myinfo()
     MainWindow.show()
     sys.exit(app.exec_())
