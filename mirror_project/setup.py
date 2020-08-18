01b6eb4a03904fe79d33acc0b5654278892a7d83
my0614 <aqi222@naver.com>
2020-08-18 09:19:49 +0900

    ADD :  급식 API setup.py

---
+++ mirror_project/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='schapi',
+    version='1.2',
+    url='https://github.com/DSM-SchoolAPI/schapi-py',
+    license='Apache License 2.0',
+    author='PlanB',
+    author_email='mingyu.planb@gmail.com',
+    description='Korean School meal API',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=['schapi'],
+    install_requires=['bs4', 'aiohttp']
+)
