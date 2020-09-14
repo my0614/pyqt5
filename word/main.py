3ecb8cb45dc5bc59bc558a6eab7a42e30afef69d
my0614 <aqi222@naver.com>
2020-09-14 16:49:45 +0900

    ADD :첫페이지로 이동

--- word/main.py
+++ word/main.py
@@ -1,25 +1,29 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from word import *
-from add_page import *
+from PyQt5 import QtGui
+import sys
+from PyQt5.QtCore import pyqtSignal, QObject
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from start_page import *
+from first_page import *
+
+
+class Main(QtWidgets.QStackedWidget):
+    pager = pyqtSignal(int)
+    def __init__(self):
+        super().__init__()
+        self.pager.connect(self.setCurrentIndex) 
+        self.start_page = Ui_start_page(self.pager)
+        self.addWidget(self.start_page) #index 0
+        self.resize(800,800)
         
-def event(self):
-    self.add.clicked.connect(self.add_click)
+        
 
-def add_click(self):
-    #self.pager.connect(self.add_page)
-    print('hello')
-Ui_MainWindow.event = event
-Ui_MainWindow.add_click = add_click
+        
 
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    ui.event()
-    ui.add_click()
-    MainWindow.show()
-    sys.exit(app.exec_())
+    ui = Main()
+    ui.show()
+    app.exec_()
