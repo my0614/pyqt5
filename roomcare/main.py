959b936d8da2bef1463da8ebaaf86264388405d0
my0614 <aqi222@naver.com>
2020-10-08 19:02:04 +0900

    add : 온습도센서 값 읽어오기

--- roomcare/main.py
+++ roomcare/main.py
@@ -1,6 +1,8 @@
 from roomcare import *
 import serial
 import threading
+from threading import Thread
+import time
 
 L1_State=0
 L2_State=0
@@ -17,16 +19,24 @@ def signals(self):
     self.LED1.clicked.connect(self.LED1_control)
     self.LED2.clicked.connect(self.LED2_control)
     self.LED3.clicked.connect(self.LED3_control)
-    
-def dht11(self):
-    ser_data = ser.readline()
-    temp_humi_data = ser_data.decode('uft-8')
-    print(str(temp_humi_data))
-    #serial_data = ser.write(bytes(data.decode()))
-    #self.temp.setText("temp")
 
+
+    
+        
+            
+def dht11(ui):
+    while True:
+        ser_data = ser.readline().decode('utf-8')
+        if ser_data:
+            print("success")
+            print(str(ser_data))
+            ui.temp.setText(ser_data[0:4]) 
+            ui.humi.setText(ser_data[4:7])
+            time.sleep(3)  
+    
 
 def LED1_control(self):
+    
     global L1_State
     if(L1_State):
         message = ''.join(['\x02','L', 'I', 'V', 'I', 'N','G', 'N','\x03'])
@@ -81,6 +91,8 @@ def LED3_control(self):
         self.LED3.setStyleSheet("background-color : red; border-radius :15px;color : white; font: 87 16pt; font-weight : bold;")
         self.LED3.move(558,360)
         L3_State = 1
+    
+    
 
 
 
@@ -90,13 +102,17 @@ Ui_MainWindow.LED2_control = LED2_control
 Ui_MainWindow.LED3_control = LED3_control
 Ui_MainWindow.dht11 = dht11
 
+
 if __name__=="__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
+    
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     ui.signals()
-    ui.dht11()
+    th = threading.Thread(target = dht11,args=(ui,))
+    th.daemon = True
+    th.start()
     MainWindow.show()
     sys.exit(app.exec_())
