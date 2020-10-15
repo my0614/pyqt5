cc7ae53bda2e4e48cedf42e0ddd0233e30656343
my0614 <aqi222@naver.com>
2020-10-15 09:54:23 +0900

    add : puqt5 GUI 파일

---
+++ dht_graph/dht11_graph.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\dht11_graph.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(831, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.title = QtWidgets.QLabel(self.centralwidget)
+        self.title.setGeometry(QtCore.QRect(360, 10, 161, 71))
+        self.title.setStyleSheet("font: 75 18pt \"Agency FB\";")
+        self.title.setObjectName("title")
+        self.graphicsView = PlotWidget(self.centralwidget)
+        self.graphicsView.setGeometry(QtCore.QRect(30, 90, 381, 451))
+        self.graphicsView.setObjectName("graphicsView")
+        self.graphicsView_2 = PlotWidget(self.centralwidget)
+        self.graphicsView_2.setGeometry(QtCore.QRect(430, 90, 381, 451))
+        self.graphicsView_2.setObjectName("graphicsView_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 831, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.title.setText(_translate("MainWindow", "DHT11_graph"))
+from pyqtgraph import PlotWidget
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
