012d44b0151246b4b053ae9a519c0c1bf44e9ce7
my0614 <aqi222@naver.com>
2020-11-04 10:27:02 +0900

    add : ballswitchsensor 사용

---
+++ ballswitch/ballswitch.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\ballswitch.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(240, 90, 351, 71))
+        self.label.setStyleSheet("font: 75 20pt \"Agency FB\";")
+        self.label.setTextFormat(QtCore.Qt.PlainText)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(280, 170, 261, 121))
+        self.label_2.setStyleSheet("background-color: rgb(250, 250, 250);\n"
+"")
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Ball Switch Counter Program"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ ballswitch/main.py
@@ -0,0 +1,49 @@
+from ballswitch import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+import pyqtgraph as pg
+import threading
+import serial
+import time
+
+
+ser = serial.Serial(port='COM7',
+                    baudrate=9600,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    timeout=1)
+
+def signals(self):
+    cnt = 0
+    
+    
+def ballsensor(self):
+    while True:
+        ser_data = ser.readline()
+        if(ser_data):
+            print("success")
+            print(ser_data)
+            cnt = int(ser_data[0:4]) # 슬라이싱한값을 int로 바로 변환
+            print(cnt)
+            self.label_2.setText(str(cnt))
+
+
+
+
+Ui_MainWindow.signals = signals
+Ui_MainWindow.ballsensor = ballsensor
+
+
+if __name__=="__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    ui.signals()
+    th = threading.Thread(target = ballsensor,args=(ui,))
+    th.daemon = True
+    th.start()
+   
+    MainWindow.show()
+    sys.exit(app.exec_())
