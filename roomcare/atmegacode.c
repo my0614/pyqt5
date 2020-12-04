6a7d4b9526a7682231c42cf79eccf3d647ca6e34
minyoung <aqi222@naver.com>
2020-12-04 16:41:14 +0900

    최종 c file

--- roomcare/atmegacode.c
+++ roomcare/atmegacode.c
@@ -1,127 +1,117 @@
-
 #define F_CPU 16000000UL
 #define BAUDRATE(x) ((F_CPU / 16 / x) - 1)
-#include <stdio.h>
-#include <string.h>
-#include <util/delay.h>
+
 #include <avr/io.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
+
+#define DHT 0
+#define DHT_DDR DDRF
+#define DHT_PORT PORTF
+#define DHT_PIN PINF
 
 int state = 0;
 char arr[15];
 char data2[10];
 int i=0;
+
 void uart_init(unsigned int baud)
 {
+	
 	UBRR0H = (unsigned char)(baud >> 8);
 	UBRR0L = (unsigned char)baud;
-	UCSR0B = (1 << TXEN0) | (1 << RXEN0) |			(1<<RXCIE0);
-}
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0) | (1<<RXCIE0);
+	
+} // 8bit, no parity, 1 stop bit, TX enable, RX ISR enable
 
 void uart_write(unsigned char data)
 {
 	while (!(UCSR0A & (1 << UDRE0)));
-	UDR0 = data;
+	// wait for sending
+	UDR0 = data; // send
 }
-#define DHT11_PIN 6
-uint8_t c=0,I_RH,D_RH,I_Temp,D_Temp,CheckSum;
 
-void Request()				// ATMega128로 스타트 펄스 전달 & 응답 과정
+void uart_string(char* str)
 {
-	DDRD |= (1<<DHT11_PIN);
-	PORTD &= ~(1<<DHT11_PIN);	// PD4 LOW
+	while (*str)
+	uart_write(*str++);
+}
+
+
+
+ISR(USART0_RX_vect)
+{
+	unsigned char buf = UDR0;
+	switch(buf)
+	{
+		case 0x02 : state = 1; return;
+		case 0x03 : state = 0; i = 0;break;
+	}
+	if(state == 1)
+	{
+		arr[i] = buf;
+		i++;
+	}
+	
+	
+}
+
+void Request()
+{
+	DHT_DDR |= (1<<DHT);
+	DHT_PORT &= ~(1<<DHT);
 	_delay_ms(20);
-	PORTD |= (1<<DHT11_PIN);	// PD4 HIGH
-}
-
-void Response()				// 온습도 센서로부터 응답
-{
-	DDRD &= ~(1<<DHT11_PIN);     // PD4 LOW
-	while(PIND & (1<<DHT11_PIN));
-	while((PIND & (1<<DHT11_PIN))==0);
-	while(PIND & (1<<DHT11_PIN));
+	DHT_PORT |= (1<<DHT);
+	_delay_us(40);
+	
+	DHT_DDR &= ~(1<<DHT);
+	while(DHT_PIN & (1<<DHT));
+	while((DHT_PIN & (1<<DHT))==0);
+	while(DHT_PIN & (1<<DHT));
 }
 
 uint8_t Receive_data()
 {
-	for (int q=0; q<8; q++)
+	uint8_t c = 0;
+	for (int i = 0; i < 8; i++)
 	{
-		while((PIND & (1<<DHT11_PIN)) == 0);  //비트가 0인지 1인지 체크
+		while((DHT_PIN & (1<<DHT)) == 0);  	/* check received bit 0 or 1 */
 		_delay_us(30);
-		if(PIND & (1<<DHT11_PIN))    //HIGH가 30ms보다 크면
-		c = (c<<1)|(0x01);	      //HIGH 상태
-		else
-		c = (c<<1);                    //LOW 상태
-		while(PIND & (1<<DHT11_PIN));
+		if(DHT_PIN & (1<<DHT)) 				/* if high pulse is greater than 30ms */
+		{
+			c = (c<<1)|(0x01);					/* then its logic HIGH */
+		}
+		else									/* otherwise its logic LOW */
+		{
+			c = (c<<1);
+		}
+
+		while(DHT_PIN & (1<<DHT));
 	}
 	return c;
 }
-void dht11()
-{
-	
-	Request();		         //시작 펄스 신호 보냄
-	Response();		         //센서로부터 응답 받음
-	data2[0] = '0x02';
-	I_RH=Receive_data();         //습도의 정수 부분
-	data2[1] = I_RH;
-	D_RH=Receive_data();	         //습도의 실수 부분
-	data2[2] = D_RH;
-	I_Temp=Receive_data();
-	data2[3] = I_Temp;	         //온도의 정수 부분
-	D_Temp=Receive_data();
-	data2[4] = D_Temp;         //온도의 실수 부분
-	CheckSum=Receive_data();       //모든 세그먼트의 체크섬
-	data2[5] = CheckSum;
-	data2[6] = '0x03';
-	
-	if ((I_RH + D_RH + I_Temp + D_Temp) != CheckSum)
-	{
-		//에러
-		
-	}
-	
-	else
-	{
-		//itoa(I_RH,data2[0],10);
-		UDR0 = data2[0];
-		
-		//itoa(D_RH,data2[1],10);
-		UDR0 = data2[1];
 
-		//itoa(I_Temp,data2[2],10);
-		UDR0 = data2[2];
-		
-		//itoa(D_Temp,data2[3],10);
-		UDR0 = data2[3];
-		
-		//itoa(CheckSum,data2[4],10);
-		UDR0 = data2[4];
-		
-	
-	
-	}
-	
-}
 int main(void)
 {
-	DDRC = 0xff;
-	DDRF = 0xff;
-	DDRD = 0xff;
-	
+	uint16_t I_Humi, D_Humi, I_Temp, D_Temp, check;
+	DDRE = 0xff;
+	DDRA = 0xFF;
+	DDRC = 0xFF;
 	uart_init(BAUDRATE(9600));
+	
 	sei();
-	while(1)
+	;
+	while (1)
 	{
-		
 		if(strcmp(arr,"LIVINGN") ==0)
 		{
-			PORTF = 0x00;
+			PORTE = 0x00;
 			memset(arr,0,15);
 		}
 		
 		if(strcmp(arr,"LIVINGF") ==0)
 		{
-			PORTF = 0xff;
+			PORTE = 0xff;
 			memset(arr,0,15);
 		}
 		
@@ -150,27 +140,37 @@ int main(void)
 			PORTD = 0xff;
 			memset(arr,0,15);
 		}
+		//uart_string("start");
+		Request();					// 데이터 전송 요청
 		
-		_delay_ms(100);
+		I_Humi = Receive_data();		// 습도 상위 8비트 전송
 		
+		D_Humi = Receive_data();		// 습도 하위 8비트 전송
+		I_Temp = Receive_data();		// 온도 상위 8비트 전송
+		D_Temp = Receive_data();		// 온도 하위 8비트 전송
+		check = Receive_data();	// CheckSum 비트 = 습도 16비트 + 온도 16비트
+		
+		if ((I_Humi + D_Humi + I_Temp + D_Temp) != check)
+		{
+			
+		}
+		else
+		{
+			
+			
+			char str[16] = {0,};
+			
+			itoa(I_Temp, str, 10);
+			itoa(D_Temp, str, 10);
+			
+			itoa(I_Humi, str, 10);
+			itoa(D_Humi, str, 10);
+			sprintf(str, "%3d%3d", I_Temp, I_Humi);
+			uart_string(str);
+		}
+		_delay_ms(3000);
+		
+		
+
 	}
 }
-
-
-ISR(USART0_RX_vect)
-{
-	unsigned char buf = UDR0;
-	switch(buf)
-	{
-		case 0x02 : state = 1; return;
-		case 0x03 : state = 0; i = 0;break;
-	}
-	if(state == 1)
-	{
-		arr[i] = buf;
-		i++;
-	}
-	
-	
-}
-
